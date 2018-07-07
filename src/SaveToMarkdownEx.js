426ce6f6aa5958c755842d3e0b0ddf596e22039f
Altair Wei <Altair_wei@outlook.com>
2018-07-07 14:10:03 +0800

    增加导出富文本表格为HTML标签的功能

--- src/SaveToMarkdownEx.js
+++ src/SaveToMarkdownEx.js
@@ -6,10 +6,12 @@ import {
 } from './WizInterface';
 const html2markdown = require('./lib/html2markdown/index');
 const he = require('he');
+const beautify_html = require('js-beautify').html
+
 
 function wizImageToMarkdown(html) {
     const imgArray = objCommon.HtmlExtractTags(html, 'img', '', '');
-    for ( imgStr of imgArray ) {
+    for (let imgStr of imgArray ) {
         const title = objCommon.HtmlTagGetAttributeValue(imgStr, 'title');
         const src = objCommon.HtmlTagGetAttributeValue(imgStr, 'src');
         const imgMarkdown = `![${title}](${src})`;
@@ -21,6 +23,18 @@ function wizImageToMarkdown(html) {
 
 function wizTableToMarkdown(html) {
     //TODO: 简单表格转换成Markdown语法，复杂表格将其prettier美化后再encode
+    const wizTableContainerArray = objCommon.HtmlExtractTags(html, 'div', 'class', 'wiz-table-container');
+    for (let tableContainer of wizTableContainerArray) {
+        // 美化
+        let table = objCommon.HtmlExtractTags(tableContainer, 'table', '', '')[0];
+        table = beautify_html(table, { indent_size: 2 });
+        table = '\n' + table + '\n';
+        table = he.encode(table, { 'allowUnsafeSymbols': true, 'encodeEverything': true }); // 避免被解析
+        // 替换
+        const tableRegex = new RegExp(tableContainer, 'g');
+        html = html.replace(tableRegex, table);
+    }
+    return html;
 }
 
 function embedImagesToMarkdown(destFileName, text, charset) {
@@ -47,20 +61,14 @@ export default function convertDocToMarkdown(doc, filePath, charset, doEmbedImag
         // 解析ziw
         const html = doc.GetHtml();
         let body = objCommon.HtmlExtractTags(html, 'body', '', '')[0];
+        body = wizTableToMarkdown(body);
         let text = html2markdown(body);
         // 最后后处理实体字符避免解析错误
         body = body.replace(/\s|&nbsp;/g, '\u0020'); // 将空格统一为转化成半角空格
         text = he.decode(text); // 处理其他实体字符
         // 导出文档
         objCommon.SaveTextToFile(destFileName, text, charset);
-
-        // 将图片编码成base64
-        /*
-        if ( doEmbedImages ) {
-            embedImagesToMarkdown(destFileName, text, charset);
-        }
-        */
-
+        
         return true;
     } else {
         WizBubbleMessage('转化失败', '参数不合法！');
